0d0525bf4a01e29ab5fb6997949f1336d5ed5932
Taj-786 <tajuddinshaik786r@gmail.com>
2025-04-29 21:47:14 +0530

    About page Changes

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function About() {
@@ -79,8 +78,8 @@ export default function About() {
                 </Link>
 
             </div>
-            <Image 
-            src="/globe.avif" 
+            <img 
+            src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Trecking"
             width={500}
             height={500}
